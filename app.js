9528d589ffaedf1b16c2c11df3da895cb7b1a575
Emerson Doyah <emerson.doyah@gmail.com>
2019-09-17 01:25:34 -0500

    fix styling

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 
 $('.shape').click(function(){
+    let activeElement = $('.active');
+    activeElement.removeClass('active');
     let element = $(this);
     let text = element.text().trim();
     element.addClass('active');
